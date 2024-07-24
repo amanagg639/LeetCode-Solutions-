0a138d93ff9825d2d8093f84636edd2f343d0166
amanagg639 <102623658+amanagg639@users.noreply.github.com>
2024-07-24 16:13:52 +0530

    Time: 1 ms (93.78%), Space: 44.6 MB (15.7%) - LeetHub

---
+++ 0046-permutations/0046-permutations.java
@@ -0,0 +1,24 @@
+class Solution {
+    public void findpermu(int []nums, List<List<Integer>> ans, List<Integer> ds, boolean[] map){
+        if(ds.size() == nums.length){
+            ans.add(new ArrayList<>(ds));
+            return;
+        }
+        for(int i=0; i<nums.length; i++){
+            if(!map[i]){
+                map[i] = true;
+                ds.add(nums[i]);
+                findpermu(nums, ans, ds, map);
+                ds.remove(ds.size() - 1);
+                map[i] = false;
+            }
+        }
+    }
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> ds = new ArrayList<>();
+        boolean[] map = new boolean[nums.length];
+        findpermu(nums, ans, ds, map);
+        return ans;
+    }
+}
