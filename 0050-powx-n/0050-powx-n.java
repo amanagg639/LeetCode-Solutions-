99185501941484d8a2a3159f4793b89317311a37
amanagg639 <102623658+amanagg639@users.noreply.github.com>
2025-01-06 05:40:01 +0530

    Time: 0 ms (100%), Space: 41.8 MB (82.71%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,18 +1,21 @@
 class Solution {
     public double myPow(double x, int n) {
-        double ans = 1.0;
-    long nn = n;
-    if (nn < 0) nn = -1 * nn;
-    while (nn > 0) {
-      if (nn % 2 == 1) {
-        ans = ans * x;
-        nn = nn - 1;
-      } else {
-        x = x * x;
-        nn = nn / 2;
-      }
+        if(n==0)return 1.0;
+        if(n==1)return x;
+        if(n<0){
+            x = 1/x;
+            n = -n;
+        }
+        return helper(x, n);
     }
-    if (n < 0) ans = (double)(1.0) / (double)(ans);
-    return ans;
+    public double helper(double x, int n){
+        if(n==0)return 1.0;
+        double half = helper(x, n/2);
+        if(n % 2 == 0){
+            return half*half;
+        }
+        else{
+            return half*half*x;
+        }
     }
 }
