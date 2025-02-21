587fc254fb808ec7b8794dede83fe453ac5497ee
amanagg639 <102623658+amanagg639@users.noreply.github.com>
2025-02-21 18:40:51 +0530

    Time: 7 ms (66.4%), Space: 86.3 MB (16.33%) - LeetHub

--- 2691-count-vowel-strings-in-ranges/2691-count-vowel-strings-in-ranges.java
+++ 2691-count-vowel-strings-in-ranges/2691-count-vowel-strings-in-ranges.java
@@ -1,32 +1,21 @@
 class Solution {
     public int[] vowelStrings(String[] words, int[][] queries) {
-        HashMap<Integer, Integer> map = new HashMap<>();
-        int sum = 0;
-        int ans[] = new int[queries.length];
+        int[] prefixSum = new int[words.length+1];
         for(int i=0; i<words.length; i++){
-            if(check(words[i])){
-                sum++;
-            }
-                map.put(i, sum);
+            prefixSum[i+1] = prefixSum[i] + (isVowel(words[i]) ? 1 : 0);
         }
-        for(int i=0; i<queries.length; i++){
-            int ind1 = queries[i][0];
-            int ind2 = queries[i][1];
-            if(check(words[ind1])){
-                ans[i] = map.get(ind2)-map.get(ind1)+1;
-            }
-            else ans[i] = map.get(ind2)-map.get(ind1);
+        int ans[] = new int[queries.length];
+        for(int i=0; i<ans.length; i++){
+            int first=queries[i][0];
+            int last = queries[i][1];
+            ans[i] = prefixSum[last+1] - prefixSum[first];
         }
         return ans;
     }
-    public boolean check(String str){
-        String vowel = "aeiou";
-        String first = "";
-        first += str.charAt(0);
-        String last = "";
-        last += str.charAt(str.length()-1);
-        if(vowel.contains(first) && vowel.contains(last))
-        return true;
-        else return false;
+    public boolean isVowel(String str){
+        return vowel(str.charAt(0)) && vowel(str.charAt(str.length()-1));
+    }
+    public boolean vowel(char c){
+        return "aeiou".indexOf(c) != -1;
     }
 }
